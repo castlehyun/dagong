dad67df6320c7569255c58a752660b152af351cf
castlehyun <md05108@naver.com>
2022-09-02 00:08:38 +0900

    투두리스트 화면 확인이 안돼서 로그인 화면 재활용하며 확인 중

--- screens/todolist.js
+++ screens/todolist.js
@@ -2,25 +2,121 @@ import React from 'react';
 import {StyleSheet, ScrollView} from 'react-native';
 import TodoListItem from './TodoListItem';
 
-const TodoList = ({todos, onRemove, onToggle}) => {
+import React, {useState} from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {StatusBar} from 'expo-status-bar';
+//formik
+import {Formik} from 'formik';
+
+//icons
+import {Octicons, Ionicons} from '@expo/vector-icons';
+
+
+import {View} from 'react-native';
+
+import {
+    StyledContainer,
+    InnerContainer,
+    PageLogo,
+    PageTitle,
+    SubTitle,
+    StyledFormArea,
+    LeftIcon,
+    StyledInputLabel,
+    StyledTextInput,
+    RightIcon,
+    StyledButton,
+    ButtonText,
+    Colors,
+    MsgBox,
+    Line,
+    ExtraText,
+    ExtraView,
+    TextLink,
+    TextLinkContent,
+} from './../components/styles';
+
+//Colors
+const {brand,darkLight} = Colors;
+
+const TodoList=({navigation})=>{
+  const [hidePassword, setHidePassword] = useState(true);
   return (
-    <ScrollView contentContainerStyle={styles.listContainer}>
-      {todos.map(todo => (
-        <TodoListItem
-          key={todo.id}
-          {...todo}
-          onRemove={onRemove}
-          onToggle={onToggle}
-        />
-      ))}
-    </ScrollView>
+      <StyledContainer>
+          <StatusBar style="dark" />
+          <InnerContainer>
+              <PageLogo resizeMode="cover" source={require('./../assets/logo.png')} />
+              <PageTitle>Dagong</PageTitle>
+              <SubTitle>할 일 리스트</SubTitle>
+
+              <Formik
+                  initialValues={{email:'',password:''}}
+                  onSubmit={(values)=>{
+                      console.log(values);
+                      navigation.navigate("Welcome");
+                  }}
+              >
+                  {({handleChange, handleBlur, handleSubmit, values})=>(
+                  <StyledFormArea>
+                      <MyTextInput
+                          label="Todolist"
+                          icon="heart"
+                          placeholder="수학 공부 하기"
+                          placeholderTextColor={darkLight}
+                          onChangeText={handleChange('Todolist')}
+                          onBlur={handleBlur('Todolist')}
+                          value={values.Todolist}
+                          secureTextEntry={hidePassword}
+                          isPassword={true}
+                          hidePassword={hidePassword}
+                          setHidePassword={setHidePassword}
+                      />
+                      <MsgBox>...</MsgBox>
+                      <StyledButton onPress={handleSubmit}>
+                          <ButtonText>
+                              할 일 등록
+                          </ButtonText>
+                      </StyledButton>
+                      <StyledButton onPress={()=>navigation.navigate("TodoList")}>
+                          <ButtonText>
+                              할 일 확인
+                          </ButtonText>
+                      </StyledButton>
+                      <Line />
+
+                      {/*<ExtraView>
+                          <ExtraText>아직 계정이 없으신가요?</ExtraText>
+                          <TextLink onPress={()=>navigation.navigate("Signup")}>
+                              <TextLinkContent>회원가입</TextLinkContent>
+                          </TextLink>    
+                  </ExtraView>*/}
+                  </StyledFormArea>
+                  )}
+
+              </Formik>
+
+          </InnerContainer>
+      </StyledContainer>
   );
 };
 
-const styles = StyleSheet.create({
-  listContainer: {
-    alignItems: 'center',
-  },
-});
+const MyTextInput=({label, icon, isPassword,hidePassword,setHidePassword, ...props})=>{
+  return (
+      <View>
+          <LeftIcon>
+              <Octicons name={icon} size={30} color={brand}/>
+          </LeftIcon>
+          <StyledInputLabel>{label}</StyledInputLabel>
+          <StyledTextInput {...props} />
+          {isPassword && (
+              <RightIcon onPress={()=>setHidePassword(!hidePassword)}>
+                  <Ionicons name={hidePassword ? 'md-eye-off': 'md-eye'} size={30} color={darkLight}/>
+              </RightIcon>
+          )}
+      </View>
+  );
+};
+
+
 
 export default TodoList;
