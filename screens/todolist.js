0cd43edfbe76c204d7a9b309f33e6a3553e8b0de
castlehyun <md05108@naver.com>
2022-09-22 21:08:01 +0900

    오류 원인: react는 렌더이 화면에 커밋된 후에야 모든 효과를 실행

--- screens/todolist.js
+++ screens/todolist.js
@@ -1,3 +1,5 @@
+/*
+
 import React, {useState} from 'react';
 import {StyleSheet, ScrollView} from 'react-native';
 import TodoListItem from './TodoListItem';
@@ -64,6 +66,10 @@ const TodoList=({navigation})=>{
       {id: Math.random().toString(),textValue:text, checked: false},
     ]);
   };
+  //check여부 = 할 일 수행 여부
+  //id는 랜덤
+
+
 
 
   return (
@@ -141,6 +147,69 @@ const MyTextInput=({label, icon, isPassword,hidePassword,setHidePassword, ...pro
   );
 };
 
+*/
 
+import React, {useState} from 'react';
+import TodoList from '../components/Todo';
+import {SafeAreaView, View, Text, StyleSheet} from 'react-native';
+import TodoInsert from '../components/TodoInsert';
+
+const App = () => {
+  // todos: {id: Number, textValue: string, checked: boolean }
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = text => {
+    setTodos([
+      ...todos,
+      {id: Math.random().toString(), textValue: text, checked: false},
+    ]);
+  };
+
+  const onRemove = id => e => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  const onToggle = id => e => {
+    setTodos(
+      todos.map(todo =>
+        todo.id === id ? {...todo, checked: !todo.checked} : todo,
+      ),
+    );
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.appTitle}>Hello Todolist</Text>
+      <View style={styles.card}>
+        <TodoInsert onAddTodo={addTodo} />
+        <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#3143e8',
+  },
+  appTitle: {
+    color: '#fff',
+    fontSize: 36,
+    marginTop: 30,
+    marginBottom: 30,
+    fontWeight: '300',
+    textAlign: 'center',
+    backgroundColor: '#3143e8',
+  },
+  card: {
+    backgroundColor: '#fff',
+    flex: 1,
+    borderTopLeftRadius: 10, // to provide rounded corners
+    borderTopRightRadius: 10, // to provide rounded corners
+    marginLeft: 10,
+    marginRight: 10,
+  },
+});
 
 export default TodoList;
