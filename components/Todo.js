c5ede48a3d12475c09a289837ad71c4cdde63484
castlehyun <md05108@naver.com>
2022-09-28 00:02:02 +0900

    undefine map함수 해결 중

--- components/Todo.js
+++ components/Todo.js
@@ -3,6 +3,13 @@ import {StyleSheet, ScrollView} from 'react-native';
 import TodoListItem from './TodoListItem';
 
 const TodoList = ({todos, onRemove, onToggle}) => {
+  //undefined is not an object(evaluating 'todos.map')
+  todo={
+    id: null,
+    pw: null
+  };
+  //todo 변수 만들어야 하는지..
+
   return (
     <ScrollView contentContainerStyle={styles.listContainer}>
       {todos.map(todo => (
