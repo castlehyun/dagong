e39d79f5129c65ced70d5f68f2d6c50f6010ae7a
castlehyun <md05108@naver.com>
2022-11-01 20:49:53 +0900

    css 적용하는 중

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import {MdCheckBox, MdCheckBoxOutlineBlank} from 'react-icons/md';
 
 const TodoItem = ({todo}) =>{
-  const {} = todo;
-return <Text>{text}</Text>;
+  const {id, text, checked} = todo;
+return (<View className="TodoItem">
+  <View className={`content ${checked ? "checked": ""}`}>
+    {checked ? <MdCheckBox/> : <MdCheckBoxOutlineBlank />}
+    <View>{text}</View>
+  </View>
+</View>
+);
 };
 
 export default TodoItem;
