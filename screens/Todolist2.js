1f465ccb23f9e04691e6a8d61037811711b4cb86
castlehyun <md05108@naver.com>
2022-11-02 22:52:53 +0900

    할 일 입력/버튼 추가

--- screens/Todolist2.js
+++ screens/Todolist2.js
@@ -1,14 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import Title from '../components/Title';
 import React from 'react';
-import { StyleSheet, View, Text, SafeAreaView } from 'react-native';
+import { StyleSheet, View, Text, SafeAreaView, Button } from 'react-native';
+import { useState } from 'react';
+import Input from '../components/Input';
 
 export default function Todolist2() {
+    const [newTask, setNewTask]=useState('');
+
+    const _addTask=()=>{
+        alert(`add : ${newTask}`);
+        setNewTask('');
+    };
+
+    const _handleTextChange = (text) =>{
+        setNewTask(text);
+    };
+
     return (
         <SafeAreaView style={styles.container}>
-            <Title title="todo list"></Title>
-            <Text>start</Text>
             <StatusBar style='auto' />
+            <Title title="🍪Todo list🍪"></Title>
+            <Input
+                value={newTask}
+                onChangeText={_handleTextChange}
+                onSubmitEditing={_addTask}
+            />
+            <Button
+                title="enter"
+                onPress={()=>{
+                    _addTask();
+                }}
+            />
         </SafeAreaView>
     );
 }
