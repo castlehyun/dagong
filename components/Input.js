1efafc09934ac75767e36299507eef468dcc7a12
castlehyun <md05108@naver.com>
2022-11-02 22:56:15 +0900

    추가한 할 일 alert에서도 볼 수 있음.

--- components/Input.js
+++ components/Input.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { Dimensions, StyleSheet, Text, TextInput } from "react-native";
 
-const Input=()=>{
+const Input=({ value, onChangeText, onSubmitEditing})=>{
     return (
-        <TextInput style={StyleSheet.input} placeholder="+ 할 일 추가" maxLength={50} />
+        <TextInput 
+            style={StyleSheet.input} 
+            placeholder="+ 할 일 추가" 
+            maxLength={50}
+            value={value}
+            onChangeText={onChangeText}
+            onSubmitEditing={onSubmitEditing}
+            />
     );
 };
 
