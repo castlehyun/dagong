91e15b60892be6e6480a6f37beb03e74745a9e85
tteum <set990317@naver.com>
2022-11-11 22:09:46 +0900

    Modify stopwatch design

--- screens/StopWatchAPI.js
+++ screens/StopWatchAPI.js
@@ -30,43 +30,10 @@ import {
 } from './../components/styles';
 
 const StopWatchAPI = () => {
-  
-  // this.state = {
-  //   timerStart: false,
-  //   stopwatchStart: false,
-  //   totalDuration: 90000,
-  //   timerReset: false,
-  //   stopwatchReset: false,
-  // };
-
-  // this.toggleTimer = this.toggleTimer.bind(this);
-  // this.resetTimer = this.resetTimer.bind(this);
-  // this.toggleStopwatch = this.toggleStopwatch.bind(this);
-  // this.resetStopwatch = this.resetStopwatch.bind(this);
 
   const [start, setStart] = useState(false);
   const [reset, setReset] = useState(false);
   const [time, setTime] = useState(false);
-
-  // toggleTimer = () => {
-  //   this.setState({timerStart: !this.state.timerStart, timerReset: false});
-  // }
- 
-  // resetTimer = () => {
-  //   this.setState({timerStart: false, timerReset: true});
-  // }
- 
-  // toggleStopwatch=() => {
-  //   this.setState({stopwatchStart: !this.state.stopwatchStart, stopwatchReset: false});
-  // }
- 
-  // resetStopwatch = () => {
-  //   this.setState({stopwatchStart: false, stopwatchReset: true});
-  // }
-  
-  // getFormattedTime = (time) => {
-  //     this.currentTime = time;
-  // };
 
   toggleStopwatch = () => {
     setStart(!start);
@@ -76,8 +43,6 @@ const StopWatchAPI = () => {
   resetStopwatch = () => {
     setStart(false);
     setReset(true);
-    // console.log("10 포인트");
-    // alert("10 포인트가 적립되었습니다.");
   }
 
   return (
@@ -92,7 +57,6 @@ const StopWatchAPI = () => {
               initialValues={{email:'',password:''}}
               onSubmit={(values)=>{
                   console.log(values);
-                  alert("start 버튼 누름")
               }}
           >   
               {({handleChange, handleBlur, handleSubmit, values})=>(
@@ -101,20 +65,14 @@ const StopWatchAPI = () => {
                         <Stopwatch laps msecs  start={start}
                           reset={reset}
                           options={options}
-                          // getTime={(time) => {}} 
                           />
-                        <TouchableHighlight onPress={toggleStopwatch} style={{backgroundColor:"#2E79F2", borderRadius: 5, padding:4, margin:5}}>
-                          <Text style={{ fontSize: 30}}>{!start ? "Start" : "Stop"}</Text>
+                        <TouchableHighlight onPress={toggleStopwatch} style={{backgroundColor:"#2E79F2", borderRadius: 5, padding:4, margin:5, alignItems:"center"}}>
+                          <Text style={{ fontSize: 28, color:"#E5E7EB"}}>{!start ? "Start" : "Stop"}</Text>
                         </TouchableHighlight>
-                        <TouchableHighlight onPress={resetStopwatch} style={{backgroundColor:"#2E79F2", borderRadius: 5, padding:4, margin:5}}>
-                          <Text style={{ fontSize: 30 }}>Reset</Text>
+                        <TouchableHighlight onPress={resetStopwatch} style={{backgroundColor:"#2E79F2", borderRadius: 5, padding:4, margin:5, alignItems:"center"}}>
+                          <Text style={{ fontSize: 28, color:"#E5E7EB" }} >Reset</Text>
                         </TouchableHighlight>
                       </View> 
-                      {/* <StyledButton onPress={()=>navigation.navigate("Welcome")}>
-                            <ButtonText>
-                                main 으로 돌아가기
-                            </ButtonText>
-                        </StyledButton> */}
                   <Line />
 
               </StyledFormArea>
@@ -124,31 +82,20 @@ const StopWatchAPI = () => {
 
       </InnerContainer>
     </StyledContainer>
-    // <View>
-    //   <Stopwatch laps msecs  start={start}
-    //     reset={reset}
-    //     options={options}
-    //     getTime={(time) => {}} />
-    //   <TouchableHighlight onPress={toggleStopwatch}>
-    //     <Text style={{ fontSize: 30 }}>{!start ? "Start" : "Stop"}</Text>
-    //   </TouchableHighlight>
-    //   <TouchableHighlight onPress={resetStopwatch}>
-    //     <Text style={{ fontSize: 30 }}>Reset</Text>
-    //   </TouchableHighlight>
-    // </View>
   )
 }
 
 const options = {
   container: {
-    backgroundColor: '#000',
+    alignItems:"center",
+    backgroundColor: '#afc8f0',
     padding: 5,
     borderRadius: 5,
-    width: 220,
+    width: 260,
   },
   text: {
     fontSize: 30,
-    color: '#FFF',
+    color: '#000000',
     marginLeft: 7,
   }
 };
